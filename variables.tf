e6245f3877626572a4ac17995f6d4dab7e66d9a7
Shubham1996-aws <69294193+Shubham1996-aws@users.noreply.github.com>
2022-08-24 11:07:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,16 @@
 variable "aws_region" {
-  default = "ap-south-1"
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  default = "AKIAX5WIIHNEEGZA7JH4"
+}
+
+variable "secret_key" {
+  default = "gUyjcGu9wHv15TITDPJR9Znkw7o12aZKiWk9YdD4"
 }
 
 variable "cluster-name" {
-  default = "micro_service_eks"
+  default = "shubham-eks"
   type    = string
 }
